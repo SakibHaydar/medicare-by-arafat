be4815fae7957c8cb7c274d379d253ef688d7905
Sakib Haydar <haydarsakib20@gmail.com>
2026-06-03 20:35:34 +0600

    fix: correct import path for SanityImageSource type

--- src/lib/image.ts
+++ src/lib/image.ts
@@ -1,6 +1,6 @@
 import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url";
 import { client } from "../../sanity/lib/client";
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 const builder = imageUrlBuilder(client);
 
